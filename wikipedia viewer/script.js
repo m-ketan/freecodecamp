6611f61a2dfb78db4b5371141b09b9b3abc001a0
ketan <code.ketan@gmail.com>
2017-04-11 15:14:52 +0530

    Third Commit

--- wikipedia viewer/script.js
+++ wikipedia viewer/script.js
@@ -3,25 +3,25 @@ $(document).ready(function() {
   $('#search').click(function() {
     var searchTerm = $('#searchTerm').val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm +"&format=json&callback=?";
-    // Ajax call for JSON.
+    $.ajax({
+      type: "GET",
+      url: url,
       async: false,
       dataType: "json",
       success: function(data) {
-        $('#searchResult').html(''); // Resets the html to blank before submitting search. 
-        // For loop for iterating through the JSON data.
+        $('#searchResult').html('');
         for (var i=0; i<data[1].length; i++) {
-        // Prepending needed JSON data to list(ul>li).
           $('#searchResult').prepend("<li><a href="+data[3][i]+"> "+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
         }
         
       },
       error: function(errorMessage) {
-        alert("Please check your internet connectivity");
+        alert("Error");
       }
     });
   });
   
-  // Adds animation to Wiki logo using animate.css.
+  // Adds animation to Wiki logo.
   $('#wikimg').hover(function() {
     $(this).toggleClass('animated flip');
   });
