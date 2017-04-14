e5c7b413f2870ad3d60ec9907cc1085620897106
ketan <code.ketan@gmail.com>
2017-04-14 07:55:22 +0530

    Minor stylesheet changes

--- Twitch API/script.js
+++ Twitch API/script.js
@@ -1,5 +1,3 @@
-// twitch client id: omol7urkqb3crqqb30ldsr287szbxj
-
 // RUNS JQUERY WHEN PAGE IS READY
 
 $(document).ready(function() {
@@ -35,7 +33,7 @@ function offChannel(myUrl2){
     
     
    $.getJSON(myUrl2, function(data) {
-                        $(".channel-info").append("<div class='row channel-row'><div class='col-md-12 channel'><ul><li><img src="+data.logo+"></li><li>"+data.display_name+"</li><li>Currently Offline</li><li><div class='status-btn-off'</li></ul></div></div>");   
+                        $(".channel-info").append("<div class='row channel-row'><div class='col-md-2 channel'><img src="+data.logo+"></div><div class='col-md-8 channel channel-details-off'>"+data.display_name+"<div><p>Currently Offline</p><div><div class='col-md-2 channel-btn'><div class='status-btn-off'</div></div></div>");   
                         
                     });
 }
@@ -43,18 +41,14 @@ function offChannel(myUrl2){
 function onChannel(myUrl){
     
     $.ajax ({
-    type:"GET",
-    url: myUrl,
-    success:function(data){
-    $(".channel-info").prepend("<div class='row channel-row'><div class='col-md-12 channel'><ul><li><img src="+data.stream.channel.logo+"></li><li>"+data.stream.channel.display_name+"</li><li>"+data.stream.game+": "+data.stream.channel.status+"</li><li><div class='status-btn-on'</li></ul></div></div>");
-    }, 
-    
-    error:function(errdata){
-    alert("Please check your internet connectivity");
+        type:"GET",
+        url: myUrl,
+        success:function(data){
+        $(".channel-info").prepend("<div class='row channel-row'><div class='col-md-2 channel'><img src="+data.stream.channel.logo+"></div><div class='col-md-8 channel-details-on'<h6>"+data.stream.channel.display_name+"</h6><p>"+data.stream.game+": "+data.stream.channel.status+"</p></div><div class='col-md-2 channel-btn'><div class='status-btn-on'></div></div></div>");
+        }, 
+
+        error:function(errdata){
+            alert("Please check your internet connectivity");
         }
     });
 }
-
-//                $("#t-logo").attr("src", data.stream.channel.logo);
-//                $("#t-name").html(data.stream.game);
-  //              $("#t-status").html(data.stream.channel.status);
